4aa554c456cf7a6a2e265efbce1fb15af09be9ba
Alfredo Mendoza <1930.kick@gmail.com>
2020-01-06 01:43:32 -0500

    add: User, Role & InputType Seeders

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Role;
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role = Role::where('name', 'admin')->firstOrFail();
+
+        $user = User::create([
+            'name' => 'Alfredo Mendoza',
+            'email' => '1930.kick@gmail.com',
+            'role_id' => $role->id
+        ]);
+    }
+}
